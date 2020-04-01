f83231531dd0dd94f42a83e7d3dda4532bbcde5f
Jharell Chambers <jharell.chambers@selu.edu>
2020-04-01 22:58:20 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Student.create(name: 'Luna', 
+Student.create(first_name: 'Luna', last_name: 'Chambers')
