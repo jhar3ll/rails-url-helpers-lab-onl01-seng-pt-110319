a477799cf35657f2dd9cedea7cf8f989db97c12f
Jharell Chambers <jharell.chambers@selu.edu>
2020-04-01 22:40:12 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -18,5 +18,6 @@ class StudentsController < ApplicationController
       @student = Student.find(params[:id])
       @student.active = !@student.active 
       @student.save
+      redirect_to student_path(@student)
     end 
 end
